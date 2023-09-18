d8ef2c5fe08ec6c37fcd0354c7f50cce45c63e05
JZP930506 <jzp930506@163.com>
2023-09-18 23:29:12 +0800

    ft:[cnbc-20230918] add the migrations about product

--- Intelligent.Factory.Management.API/Migrations/20230918152815_product_context.Designer.cs
+++ Intelligent.Factory.Management.API/Migrations/20230918152815_product_context.Designer.cs
@@ -3,6 +3,7 @@ using Intelligent.Factory.Management.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Intelligent.Factory.Management.API.Migrations
 {
     [DbContext(typeof(IntelligentFactoryManagementContext))]
-    partial class IntelligentFactoryManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20230918152815_product_context")]
+    partial class product_context
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Intelligent.Factory.Management.API/Migrations/20230918152815_product_context.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intelligent.Factory.Management.API.Migrations
+{
+    public partial class product_context : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "product",
+                schema: "IntelligentFactoryManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EntryCriteria = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "productItem",
+                schema: "IntelligentFactoryManagement",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductType = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Specifications = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<int>(type: "int", nullable: false),
+                    Unit = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Material = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TechnicalRequirements = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Remark = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_productItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_productItem_product_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "IntelligentFactoryManagement",
+                        principalTable: "product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_productItem_ProductId",
+                schema: "IntelligentFactoryManagement",
+                table: "productItem",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "productItem",
+                schema: "IntelligentFactoryManagement");
+
+            migrationBuilder.DropTable(
+                name: "product",
+                schema: "IntelligentFactoryManagement");
+        }
+    }
+}
