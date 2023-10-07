95e455d95ad14626cef493d4350a333c72a22686
JZP930506 <jzp930506@163.com>
2023-10-07 22:36:08 +0800

    ft:[cnbc-20231007] add the migrations about contract

--- Intelligent.Factory.Management.API/Migrations/20231007140835_contract_context.Designer.cs
+++ Intelligent.Factory.Management.API/Migrations/20231007140835_contract_context.Designer.cs
@@ -4,6 +4,7 @@ using Intelligent.Factory.Management.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Intelligent.Factory.Management.API.Migrations
 {
     [DbContext(typeof(IntelligentFactoryManagementContext))]
-    partial class IntelligentFactoryManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20231007140835_contract_context")]
+    partial class contract_context
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Intelligent.Factory.Management.API/Migrations/20231007140835_contract_context.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intelligent.Factory.Management.API.Migrations
+{
+    public partial class contract_context : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LogisticsUndertaker",
+                schema: "intelligent_factory_management",
+                table: "contract",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ShipDateTime",
+                schema: "intelligent_factory_management",
+                table: "contract",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShipType",
+                schema: "intelligent_factory_management",
+                table: "contract",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogisticsUndertaker",
+                schema: "intelligent_factory_management",
+                table: "contract");
+
+            migrationBuilder.DropColumn(
+                name: "ShipDateTime",
+                schema: "intelligent_factory_management",
+                table: "contract");
+
+            migrationBuilder.DropColumn(
+                name: "ShipType",
+                schema: "intelligent_factory_management",
+                table: "contract");
+        }
+    }
+}
